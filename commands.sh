1722cad0eb048b9ba833c776f18ef5916d857dca
Dan Harris <daniel.harris@asurion.com>
2019-12-21 11:14:41 -0500

    layout fix

--- commands.sh
+++ commands.sh
@@ -1,1 +1,3 @@
-alias gen='node node_modules/markdown-resume/bin/md2resume --pdf resume.md'
+alias gen-pdf='node node_modules/markdown-resume/bin/md2resume --pdf resume.md'
+
+alias gen-html='node node_modules/markdown-resume/bin/md2resume resume.md'
